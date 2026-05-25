6e033e353da7e1b2c16f03bf3f1473ecdb716281
AriffMedina <ariffmedina.contact@gmail.com>
2026-05-25 08:53:44 -0600

    feat(persistence): add base entities
    - Create core domain entities (Cliente, Categoria).
    - Add intermediate entity CompraProducto.
    - Implement composite key CompraProductoPK (@Embeddable and Serializable).

---
+++ market-backend-2026-3-a/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/persistence/entity/Categoria.java
@@ -0,0 +1,42 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column (name = "id_categoria")
+    private int idCategoria;
+
+    private String descripcion;
+
+    private boolean estado;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
