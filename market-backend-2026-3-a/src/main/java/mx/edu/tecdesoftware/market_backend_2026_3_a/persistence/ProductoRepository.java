f08a7ce794bca0c5576948aae5a3ba7410198875
AriffMedina <ariffmedina.contact@gmail.com>
2026-06-02 08:47:08 -0600

    feat: implement ProductoRepository and custom query methods - Add custom query methods in ProductoCrudRepository (sorted category filter and low stock check).- Create ProductoRepository to wrap CRUD operations (getAll, getByCategoria, getEscasos, getProductoById, save).

--- market-backend-2026-3-a/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/persistence/ProductoRepository.java
+++ market-backend-2026-3-a/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.crud.ProductoCru
 import mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
@@ -12,5 +13,39 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         // Se "castea" de Iterable a la Lista
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    /*
+    SELECT * FROM producto
+    WHERE cantidad_stock < ?
+    AND estado = true;
+     */
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado( cantidad, true);
+    }
+
+    //Obtener un producto dado el id
+    public Optional<Producto> getProductoById(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //Guardar un producto
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    /*
+    INSERT INTO producto (nombre, cantidad_stock, estado, id_categoria)
+    VALUES (?, ?, ?, ?);
+     */
+
+    //Eliminar por id
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
